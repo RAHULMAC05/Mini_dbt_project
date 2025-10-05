a861a43981739f088add87f02f6d2df5590ab918
RAHULMAC05 <rahulpaulsahai.15@gmail.com>
2025-10-05 15:23:26 +0530

    Added incremental Loading

--- project_1/models/silver/silver_customer.sql
+++ project_1/models/silver/silver_customer.sql
@@ -1,5 +1,15 @@
+{{
+  config(
+    materialized = 'incremental',
+    unique_key = 'customer_id'
+    )
+}}
+
 {% set min_year = 2025 %}
 
 SELECT *
        FROM {{ ref("bronze_customer")}}
     WHERE YEAR(updated_at) >= {{ min_year }}
+    {% if is_incremental() %}
+           AND updated_at > SELECT MAX(updated_at) FROM {{ this }}
+    {% endif %}
--- project_1/models/silver/silver_products.sql
+++ project_1/models/silver/silver_products.sql
@@ -1,5 +1,20 @@
-{% set col_name = ('Electronics','Clothing','Books','Sports','Home') %}
+ {{
+  config(
+    materialized = 'incremental',
+    unique_key = 'product_id',
+    on_schema_change = 'fail'
+    )
+}}
 
+{% set col_name = ('Electronics','Clothing','Books','Sports','Home') %}
+with cte as (
 SELECT *
 FROM {{ ref("bronze_products") }}
 WHERE category IN {{ col_name }}
+)
+
+SELECT * FROM 
+      cte
+   {% if is_incremental() %}
+        WHERE updated_at > SELECT MAX(updated_at) FROM {{ this }}
+   {% endif %}
